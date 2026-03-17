cc1f50474c4000bd9251062466431b207473e0c4
muhamed.alnaqli <muhamed.alnaqli@logosoft.ba>
2026-03-17 10:22:50 +0100

    - Added configuration for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://muhamedalnaqli.github.io',
   integrations: [
     tailwind({
       applyBaseStyles: false,
